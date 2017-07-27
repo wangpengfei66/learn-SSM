a9229dbb99454822a7e7996fe9629bafbf301d21
wangpengfei66 <13298477340@163.com>
2017-07-27 13:07:58 +0800

    学习quartz简单用法

---
+++ crm/crm_serviceImpl/src/main/java/com/kaishengit/quartz/jobs/MyJob.java
@@ -0,0 +1,16 @@
+package com.kaishengit.quartz.jobs;
+
+import org.quartz.Job;
+import org.quartz.JobDataMap;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+public class MyJob implements Job {
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        JobDataMap dataMap = jobExecutionContext.getMergedJobDataMap();
+        String to = (String) dataMap.get("to");
+        String message = (String) dataMap.get("message");
+        System.out.println("发送微信通知~~~~" + to + message);
+    }
+}
